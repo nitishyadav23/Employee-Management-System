309803f58c451301cd9dd10528ff739446d07d08
nitishyadav23 <nitishkr181006@gmail.com>
2025-07-10 09:50:53 +0530

    new

--- EMS/src/App.jsx
+++ EMS/src/App.jsx
@@ -3,7 +3,7 @@ import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
 import { AuthContext } from './context/AuthProvider'
-import Footer from './pages/Footer'; 
+
 
 const App = () => {
 
